8fe2ccc73568e6a1487b0def041db75012dc0d84
Dong Nguyen <dongnd@gmail.com>
2018-05-21 10:38:21 +0700

    Added some py files: hello.py ifelse.py division.py arithmetic_operators.py

---
+++ python/arithmetic_operators.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+# Python Arithmetic Operators
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
---
+++ python/division.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+# https://www.hackerrank.com/challenges/python-division/problem
+# Python Division
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
